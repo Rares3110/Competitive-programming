79c209aada64a414b3242fc5b7ccaf54296f7e8c
Rares3110 <68992409+Rares3110@users.noreply.github.com>
2021-12-01 19:21:45 +0200

    Update MaxFlow.cpp

--- Structuri folositoare/MaxFlow.cpp
+++ Structuri folositoare/MaxFlow.cpp
@@ -13,34 +13,34 @@ template <class T> class MaxFlow
 {
 public:
 	static int gfSize;
-	static vector <int> *gf;
-	static map <pair<int, int>, T> cap, flux; 
+	static vector <int>* gf;
+	static map <pair<int, int>, T> cap, flux;
 	static T rezultat;
 private:
-	static vector <int> *gft;
+	static vector <int>* gft;
 	//bfs
-	static int *nivel;
-	static bool *viz, *usefull;
+	static int* nivel;
+	static bool* viz, * usefull;
 	//drum + dfs
-	static pair <int, int> *muchie;
-	static int *tabel;
- 
+	static pair <int, int>* muchie;
+	static int* tabel;
+
 	static bool bfs()
 	{
 		fill(viz + 1, viz + 1 + gfSize, 0);
 		fill(usefull + 1, usefull + 1 + gfSize, 0);
 		queue <int> q;
 		q.push(1);
-		
+
 		viz[1] = usefull[1] = 1;
 		nivel[1] = 1;
- 
-		while(!q.empty())
+
+		while (!q.empty())
 		{
 			int nod = q.front();
 			q.pop();
- 
-			for (auto vec : gf[nod])
+
+			for (const auto& vec : gf[nod])
 				if (!viz[vec] && cap[{nod, vec}])
 				{
 					viz[vec] = 1;
@@ -48,8 +48,8 @@ private:
 					q.push(vec);
 					usefull[vec] = 1;
 				}
- 
-			for (auto vec : gft[nod])
+
+			for (const auto& vec : gft[nod])
 				if (!viz[vec] && flux[{nod, vec}])
 				{
 					viz[vec] = 1;
@@ -58,25 +58,25 @@ private:
 					usefull[vec] = 1;
 				}
 		}
-		
+
 		return viz[gfSize];
 	}
- 
-	static void drum(const int &lung)
+
+	static void drum(const int& lung)
 	{
 		T minim;
- 
+
 		if (tabel[1] == 1)
 			minim = cap[{muchie[1].first, muchie[1].second}];
 		else
 			minim = flux[{muchie[1].first, muchie[1].second}];
- 
+
 		for (int i = 2; i <= lung; i++)
 			if (tabel[i] == 1)
 				minim = min(minim, cap[{muchie[i].first, muchie[i].second}]);
 			else
 				minim = min(minim, flux[{muchie[i].first, muchie[i].second}]);
- 
+
 		for (int i = 1; i <= lung; i++)
 			if (tabel[i] == 1)
 			{
@@ -88,10 +88,10 @@ private:
 				flux[{muchie[i].first, muchie[i].second}] -= minim;
 				cap[{muchie[i].second, muchie[i].first}] += minim;
 			}
- 
+
 		rezultat += minim;
 	}
- 
+
 	static bool dfs(int nod = 1, int vf = 1)
 	{
 		if (nod == gfSize)
@@ -99,29 +99,29 @@ private:
 			drum(vf - 1);
 			return true;
 		}
-		
+
 		bool found = 0;
-		for (auto vec : gf[nod])
+		for (const auto& vec : gf[nod])
 			if (nivel[vec] > nivel[nod] && cap[{nod, vec}])
 			{
-				muchie[vf] = {nod, vec};
+				muchie[vf] = { nod, vec };
 				tabel[vf] = 1;
 				usefull[vec] = dfs(vec, vf + 1);
-				if(usefull[vec]) found = true;
+				if (usefull[vec]) found = true;
 			}
- 
-		for (auto vec : gft[nod])
+
+		for (const auto& vec : gft[nod])
 			if (nivel[vec] > nivel[nod] && flux[{nod, vec}])
 			{
-				muchie[vf] = {nod, vec};
+				muchie[vf] = { nod, vec };
 				tabel[vf] = 2;
 				usefull[vec] = dfs(vec, vf + 1);
-				if(usefull[vec]) found = true;
+				if (usefull[vec]) found = true;
 			}
- 
+
 		return found;
 	}
- 
+
 	static void deleteAll()
 	{
 		delete[] gf;
@@ -134,34 +134,34 @@ private:
 		cap.clear();
 		flux.clear();
 	}
-	static void assignAll(const int &n)
+	static void assignAll(const int& n)
 	{
-		gf = new vector <int> [n + 1]();
-		gft = new vector <int> [n + 1]();
-		nivel = new int [n + 1]();
+		gf = new vector <int>[n + 1]();
+		gft = new vector <int>[n + 1]();
+		nivel = new int[n + 1]();
 		viz = new bool[n + 1]();
-		usefull = new bool [n + 1]();
-		muchie = new pair<int, int> [n + 1]();
-		tabel = new int [n + 1]();
+		usefull = new bool[n + 1]();
+		muchie = new pair<int, int>[n + 1]();
+		tabel = new int[n + 1]();
 		rezultat = 0;
 		gfSize = n;
 	}
 public:
-	static void generate(const int &n, const vector <pair<int, T>> graf[])
+	static void generate(const int& n, const vector <pair<int, T>> graf[])
 	{
 		deleteAll();
 		assignAll(n);
- 
-		for(int i = 1; i <= n; i++)
-			for(auto vec : graf[i])
+
+		for (int i = 1; i <= n; i++)
+			for (const auto& vec : graf[i])
 			{
 				gf[i].push_back(vec.first);
 				gft[vec.first].push_back(i);
 				cap[{i, vec.first}] = cap[{vec.first, i}] = vec.second;
 			}
- 
-		while(bfs())
-			while(dfs());
+
+		while (bfs())
+			while (dfs());
 	}
 };
 template <class T> int MaxFlow<T>::gfSize = 0;
