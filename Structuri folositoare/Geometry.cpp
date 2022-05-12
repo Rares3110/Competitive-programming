85c1f439bb193ac0cd8ddb2de4577cee9a88b0b3
Rares3110 <68992409+Rares3110@users.noreply.github.com>
2022-05-12 12:20:31 +0300

    Update Geometry.cpp

--- Structuri folositoare/Geometry.cpp
+++ Structuri folositoare/Geometry.cpp
@@ -80,65 +80,81 @@ point operator * (double value, point b)
 class Line
 {
 public:
-	double x, y, offset;
+	double a, b, c;
+	//ax + by + c = 0
 
 	Line()
 	{
-		x = 1;
-		y = 0;
-		offset = 0;
+		a = 0;
+		b = 1;
+		c = 0;
 	}
-	Line(double x, double y, double offset)
+	Line(double a, double b, double c)
 	{
-		this->x = x;
-		this->y = y;
-		this->offset = offset;
+		this->a = a;
+		this->b = b;
+		this->c = c;
+	}
+	Line(point p1, point p2)
+	{
+		if (abs(p1.x - p2.x) < 0.00001)
+		{
+			b = 0;
+			a = 1;
+			c = - p1.x;
+		}
+		else
+		{
+			b = 1;
+			a = -point::panta(p1, p2);
+			c = -a * p1.x - p1.y;
+		}
 	}
 	point getPointFromX(double xValue)
 	{
-		return { xValue, -(this->x * xValue + this->offset) / this->y };
+		return { xValue, -(this->a * xValue + this->c) / this->b };
 	}
 	point getPointFromY(double yValue)
 	{
-		return { -(this->y * yValue + this->offset) / this->x, yValue };
+		return { -(this->b * yValue + this->c) / this->a, yValue };
 	}
 	bool isHorizontal()
 	{
-		if (abs(x) < 0.00001)
+		if (abs(a) < 0.00001)
 			return true;
 		return false;
 	}
 	bool isVertical()
 	{
-		if (abs(y) < 0.00001)
+		if (abs(b) < 0.00001)
 			return true;
 		return false;
 	}
 	bool onLine(point punct)
 	{
-		if (abs(x * punct.x + y * punct.y + offset) < 0.00001)
+		if (abs(a * punct.x + b * punct.y + c) < 0.00001)
 			return true;
 		return false;
 	}
 	static point pointFromIntersection(Line l1, Line l2)
 	{
-		double x = (l1.y * l2.offset - l2.y * l1.offset) / (l1.x * l2.y - l2.x * l1.y);
-		double y = (l1.offset * l2.x - l2.offset * l1.x) / (l1.x * l2.y - l2.x * l1.y);
+		double x = (l1.b * l2.c - l2.b * l1.c) / (l1.a * l2.b - l2.a * l1.b);
+		double y = (l1.c * l2.a - l2.c * l1.a) / (l1.a * l2.b - l2.a * l1.b);
 		return { x, y };
 	}
 	static bool paralel(Line l1, Line l2)
 	{
-		if (abs(l1.x * l2.y - l2.x * l1.y) < 0.00001)
+		if (abs(l1.a * l2.b - l2.a * l1.b) < 0.00001)
 			return true;
 		return false;
 	}
-	const bool operator < (const Line& b) const
+	const bool operator < (const Line& ln) const
 	{
-		if (abs(x - b.x) > 0.00001)
-			return x < b.x;
-		else if (abs(x - b.x) > 0.00001)
-			return y < b.y;
+		if (abs(a - ln.a) > 0.00001)
+			return a < ln.a;
+		else if (abs(a - ln.a) > 0.00001)
+			return b < ln.b;
 		else
-			return offset < b.offset;
+			return c < ln.c;
 	}
 };
